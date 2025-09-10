454894f7967a0c4b9d99053993d7455be05ab111
NurMohammad56 <milonsardar56@gmail.com>
2025-09-10 23:57:21 +0600

    Student service are completed

--- src/main/java/com/university/attendance/service/StudentServiceImpl.java
+++ src/main/java/com/university/attendance/service/StudentServiceImpl.java
@@ -1,37 +1,37 @@
-// package com.university.attendance.service;
+package com.university.attendance.service;
 
-// import com.university.attendance.model.Student;
-// import com.university.attendance.repository.StudentRepository;
-// import org.springframework.stereotype.Service;
-// import java.util.List;
-// import java.util.Optional;
+import com.university.attendance.model.Student;
+import com.university.attendance.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
-// @Service
-// public class StudentServiceImpl implements StudentService {
-//     private final StudentRepository studentRepository;
+@Service
+public class StudentServiceImpl implements StudentService {
+    private final StudentRepository studentRepository;
 
-//     public StudentServiceImpl(StudentRepository studentRepository) {
-//         this.studentRepository = studentRepository;
-//     }
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 
-//     @Override
-//     public Student create(Student s) {
-//         return studentRepository.save(s);
-//     }
+    @Override
+    public Student create(Student s) {
+        return studentRepository.save(s);
+    }
 
-//     @Override
-//     public List<Student> getAll() {
-//         return studentRepository.findAll();
-//     }
+    @Override
+    public List<Student> getAll() {
+        return studentRepository.findAll();
+    }
 
-//     @Override
-//     public Student getById(String id) {
-//         Optional<Student> opt = studentRepository.findById(id);
-//         return opt.orElse(null); // simple: return null if not found
-//     }
+    @Override
+    public Student getById(String id) {
+        Optional<Student> opt = studentRepository.findById(id);
+        return opt.orElse(null); // simple: return null if not found
+    }
 
-//     @Override
-//     public void delete(String id) {
-//         studentRepository.deleteById(id);
-//     }
-// }
+    @Override
+    public void delete(String id) {
+        studentRepository.deleteById(id);
+    }
+}
