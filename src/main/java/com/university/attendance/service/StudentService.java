79bd45fece9475e4128d59593cfbdb697a58dfa4
NurMohammad56 <milonsardar56@gmail.com>
2025-09-10 23:57:28 +0600

    Student service are completed

--- src/main/java/com/university/attendance/service/StudentService.java
+++ src/main/java/com/university/attendance/service/StudentService.java
@@ -1,11 +1,11 @@
-// package com.university.attendance.service;
+package com.university.attendance.service;
 
-// import com.university.attendance.model.Student;
-// import java.util.List;
+import com.university.attendance.model.Student;
+import java.util.List;
 
-// public interface StudentService {
-//     Student create(Student s);
-//     List<Student> getAll();
-//     Student getById(String id);
-//     void delete(String id);
-// }
+public interface StudentService {
+    Student create(Student s);
+    List<Student> getAll();
+    Student getById(String id);
+    void delete(String id);
+}
