99684ddf12031249b47ecfa9f570588af40088c2
NurMohammad56 <milonsardar56@gmail.com>
2025-09-10 23:56:49 +0600

    Course service and impl are completed

--- src/main/java/com/university/attendance/service/CourseServiceImpl.java
+++ src/main/java/com/university/attendance/service/CourseServiceImpl.java
@@ -1,31 +1,31 @@
-// package com.university.attendance.service;
+package com.university.attendance.service;
 
-// import com.university.attendance.model.Course;
-// import com.university.attendance.repository.CourseRepository;
-// import org.springframework.stereotype.Service;
-// import java.util.List;
+import com.university.attendance.model.Course;
+import com.university.attendance.repository.CourseRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
 
-// @Service
-// public class CourseServiceImpl implements CourseService {
+@Service
+public class CourseServiceImpl implements CourseService {
 
-//     private final CourseRepository courseRepository;
+    private final CourseRepository courseRepository;
 
-//     public CourseServiceImpl(CourseRepository courseRepository) {
-//         this.courseRepository = courseRepository;
-//     }
+    public CourseServiceImpl(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
 
-//     @Override
-//     public Course create(Course c) {
-//         return courseRepository.save(c);
-//     }
+    @Override
+    public Course create(Course c) {
+        return courseRepository.save(c);
+    }
 
-//     @Override
-//     public List<Course> getAll() {
-//         return courseRepository.findAll();
-//     }
+    @Override
+    public List<Course> getAll() {
+        return courseRepository.findAll();
+    }
 
-//     @Override
-//     public Course getById(String id) {
-//         return courseRepository.findById(id).orElse(null);
-//     }
-// }
+    @Override
+    public Course getById(String id) {
+        return courseRepository.findById(id).orElse(null);
+    }
+}
